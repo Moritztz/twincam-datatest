c459d61db6ebcfbbf70eadeb34cc0dd035372bc0
Moritztz <tm.283wings@gmail.com>
2018-07-13 15:41:36 +0900

    v

--- script_7.js
+++ script_7.js
@@ -1,3 +1,4 @@
+$(function () {
     // peer serverからpeeridもらうとサンプルレベルだと
     // id打ち込むのがめんどいので、乱数でidを生成
     var myid = Math.floor(Math.random() * 10000).toString();
@@ -11,46 +12,47 @@
     var peer = new Peer(myid, { key: "9373b614-604f-4fd5-b96a-919b20a7c24e" });
 
     // 接続元
-        $("form#connect").submit(function (ev) {
+    $("form#connect").submit(function (ev) {
         ev.preventDefault();
 
-    // 接続先のpeeridを取得し、connect()で接続処理を開始する
-    var peerid = $("#peerid").val();
-    var conn = peer.connect(peerid);
+        // 接続先のpeeridを取得し、connect()で接続処理を開始する
+        var peerid = $("#peerid").val();
+        var conn = peer.connect(peerid);
 
-    // peerとの接続が完了したらメッセージ送受信状態に移る
-            conn.on("open", function () {
-        messaging(this);
+        // peerとの接続が完了したらメッセージ送受信状態に移る
+        conn.on("open", function () {
+            messaging(this);
+        });
     });
-});
 
-// 接続先
-        peer.on("connection", function (conn) {
+    // 接続先
+    peer.on("connection", function (conn) {
         // peerからの接続要求を受け取ったら、メッセージ送受信状態に移る
         messaging(conn);
     });
 
     // メッセージ送受信を処理する関数
-        var messaging = function (conn) {
+    var messaging = function (conn) {
         // 接続処理用のフォームを隠し、
         // メッセージ送受信用フォームをフェードインする。
         // 接続処理完了時にフォームを隠すことで、接続先が connect() を呼ぶことを防いでいる。
         $("form#connect").hide();
-    $("form#messaging").fadeIn()
-                .on("submit", function (ev) {
-        ev.preventDefault(); // デフォルトのsubmit動作（reload）を抑制
+        $("form#messaging").fadeIn()
+            .on("submit", function (ev) {
+                ev.preventDefault(); // デフォルトのsubmit動作（reload）を抑制
 
-    // 送信メッセージを取得し、peerに送信する
-    var send_data = $(this).find("input").val();
-    conn.send(send_data);
+                // 送信メッセージを取得し、peerに送信する
+                var send_data = $(this).find("input").val();
+                conn.send(send_data);
 
-    // 送信フォームのメッセージをクリアする
-    $(this).find("input").val("");
+                // 送信フォームのメッセージをクリアする
+                $(this).find("input").val("");
+            });
+
+        // peerからメッセージを受信した際の処理
+        conn.on("data", function (data) {
+            // 受信メッセージを #receive にappendしていく
+            $("<p>").text(data).appendTo("#receive");
+        });
+    }
 });
-
-// peerからメッセージを受信した際の処理
-            conn.on("data", function (data) {
-        // 受信メッセージを #receive にappendしていく
-        $("<p>").text(data).appendTo("#receive");
-    });
-}
